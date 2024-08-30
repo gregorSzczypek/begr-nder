8a686b3bc1236141c9aabdad1986e0a6ad037b38
Developer <gregor.szczypek@gmail.com>
2024-08-30 21:22:46 +0200

    init

---
+++ begruender/begruender/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  begruender
+//
+//  Created by Developer on 30.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
+
+
+// AIzaSyBnf02iX6iQym6A_YjdOs5YJSw9oX70JTM gemini api key
---
+++ begruender/begruender/begruenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  begruenderApp.swift
+//  begruender
+//
+//  Created by Developer on 30.08.24.
+//
+
+import SwiftUI
+
+@main
+struct begruenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
